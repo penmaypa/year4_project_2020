857b4760c2f01cd2ccb03074b6a6e51473164b28
Penuel Maypa <penmayp@gmail.com>
2020-01-22 12:07:06 +0000

    ok it works, now its time to make it into a separate function

--- App_CEMM_Dashboard/CEMM_main.py
+++ App_CEMM_Dashboard/CEMM_main.py
@@ -6,8 +6,11 @@ import pandas as pd
 import plotly.graph_objects as go
 from cemm_lib import visual_rep_mod as visual
 
+from dash.dependencies import Input, Output
+
 import os
 #from app import app
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 
 #df = visual.read_this_csv("Data/emp2017.csv")
@@ -17,7 +20,7 @@ df = visual.read_this_csv("Data/dub_rent.csv")
 labels = ['Oxygen','Hydrogen','Carbon_Dioxide','Nitrogen']
 values = [4500, 2500, 1053, 500]
 
-fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
+# fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
 
 
 
@@ -46,11 +49,24 @@ app.layout = html.Div(children=[
             'backgroundColor':'rgb(112, 180, 255, 0.5)'
         }
     ),
+
+    # === Callback test ====
+
+    dcc.Input(id='my-id', value='initial value', type='text'),
+    html.Div(id='my-div'),
+
     # === Visual Representation here ===
     visual.generate_table("Employment 2017",df),
     visual.generate_barChart("Employment 2017",df),
-    fig.show()
+    # fig.show()
 ])
+
+@app.callback(
+    Output(component_id='my-div', component_property='children'),
+    [Input(component_id='my-id', component_property='value')]
+)
+def update_output_div(input_value):
+    return 'You\'ve entered "{}"'.format(input_value)
 
 
 
