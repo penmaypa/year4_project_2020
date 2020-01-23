15ae3b463312d71c1fa00c5abaf6a23f9c0a1163
Penuel Maypa <penmayp@gmail.com>
2020-01-23 05:33:29 +0000

    ...

--- App_CEMM_Dashboard/CEMM_main.py
+++ App_CEMM_Dashboard/CEMM_main.py
@@ -65,6 +65,7 @@ app.layout = html.Div(children=[
     Output(component_id='my-div', component_property='children'),
     [Input(component_id='my-id', component_property='value')]
 )
+
 def update_output_div(input_value):
     return 'You\'ve entered "{}"'.format(input_value)
 
