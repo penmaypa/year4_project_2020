a4eb8e4d0fdd27743ea248d5fe417b642cc28831
Penuel Maypa <penmayp@gmail.com>
2020-01-30 05:39:00 +0000

    still working on the Upload function, to be resume...

--- App_CEMM_Dashboard/CEMM_main.py
+++ App_CEMM_Dashboard/CEMM_main.py
@@ -6,28 +6,34 @@ import pandas as pd
 import plotly.graph_objects as go
 from cemm_lib import visual_rep_mod as visual
 
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 
 import os
+
+#=== Imports for Upload function ====
+import base64
+import datetime
+import io
+
 #from app import app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 
 #df = visual.read_this_csv("Data/emp2017.csv")
 #df = "empty"
-df = visual.read_this_csv("Data/dub_rent.csv")
+
+#df = visual.read_this_csv("Data/dub_rent.csv")
 
 # fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
 
 
-
-
+dbn = 0
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[
 
-    # Headings
+    # ==== Headings =====
     html.Div(
         [
             html.H1(children='CEMM Dashboard',
@@ -80,12 +86,59 @@ app.layout = html.Div(children=[
         ]
     ),
 
-    # === Visual Representation here ===
-    visual.generate_table("Employment 2017",df),
-    visual.generate_barChart("Employment 2017",df),
-    # fig.show()
+
 ])
 
+# ====  Upload Method ====
+def parse_contents(contents, filename, date):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+
+    return html.Div([
+        html.H5(filename),
+        html.H6(datetime.datetime.fromtimestamp(date)),
+
+        # === Visual Representation here ===
+        visual.generate_table("Employment 2017",df),
+        visual.generate_barChart("Employment 2017",df),
+
+        html.Hr(),  # horizontal line
+
+        # For debugging, display the raw contents provided by the web browser
+        html.Div('Raw Content'),
+        html.Pre(contents[0:200] + '...', style={
+            'whiteSpace': 'pre-wrap',
+            'wordBreak': 'break-all'
+        })
+    ])
+
+dbn+=1
+print(dbn ,"- Running Callback...")
+@app.callback(Output('output-data-upload', 'children'),
+              [Input('upload-data', 'contents')],
+              [State('upload-data', 'filename'),
+               State('upload-data', 'last_modified')])
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    print("---\n update_output() ...")
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
 
 if __name__ == '__main__':
     app.run_server(debug=True)
