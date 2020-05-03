6c2db1766cb3eb87ea3ca9c792ee09e947d6a561
Penuel Maypa <penmayp@gmail.com>
2020-05-03 05:02:27 +0100

    reset

--- Dash Tutorial/Tets/compute_class.py
+++
@@ -1,3 +0,0 @@
-def compute_this(num_1, num_2):
-    result = num_1 + num_2
-    return result
--- Dash Tutorial/Tets/main.py
+++
@@ -1,6 +0,0 @@
-import compute_class
-
-answer = compute_class.compute_this(1, 1)
-result = compute_class.compute_this(2, 2)
-print(answer)
-print(result)
